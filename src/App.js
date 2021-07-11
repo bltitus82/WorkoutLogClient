bf8e3538e9fe3d8be12f02d19ddc798690021840
Brad Titus <bltitus@me.com>
2021-07-11 16:13:35 -0400

    10.9.2 React WOL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, useEffect, useState } from 'react';
 import Sitebar from './home/Navbar';
 import Auth from './auth/Auth';
+import WorkoutIndex from './workouts/WorkoutIndex';
 
 function App() {
   const [sessionToken, setSessionToken] = useState('');
@@ -22,10 +23,14 @@ function App() {
     setSessionToken('');
   }
 
+  const protectedViews = () => {
+    return (sessionToken === localStorage.getItem('token') ? <WorkoutIndex token={sessionToken}/> : <Auth updateToken={updateToken}/>)
+  }
+
   return (
     <div>
-      <Sitebar />
-      <Auth updateToken={updateToken} />
+      <Sitebar clearToken={clearToken}/>
+      {protectedViews()}
     </div>
   );
 }
